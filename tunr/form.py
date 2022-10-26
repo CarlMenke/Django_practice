fa757ea982f04e8781420e111fe77bace8b65447
carlmenke <carlmenke13@gmail.com>
2022-10-25 17:33:54 -0700

    added routes and redirect with Django

---
+++ tunr/form.py
@@ -0,0 +1,9 @@
+# tunr/forms.py
+from django import forms
+from .models import Artist, Song
+
+class ArtistForm(forms.ModelForm):
+
+    class Meta:
+        model = Artist
+        fields = ('name', 'photo_url', 'nationality',)
--- tunr/urls.py
+++ tunr/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.artist_list, name='artist_list'),
     path('songs/', views.song_list, name="song_list"),
-    path('artists/<int:pk>', views.artist_detail, name="artist_detail")
+    path('artists/<int:pk>', views.artist_detail, name="artist_detail"),
+    path('artist/new', views.artist_create, name='artist_create'),
+    path('artists/<int:pk>/edit', views.artist_edit, name='artist_edit'), 
+    path('artists/<int:pk>/delete', views.artist_delete, name='artist_delete'),
 ]
